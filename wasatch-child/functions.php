30b9d11c1fc74296eec4ecb2bb8b811d7c52c6eb
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:13:51 -0600

    changed version number to purge style sheet

--- wasatch-child/functions.php
+++ wasatch-child/functions.php
@@ -6,7 +6,7 @@ function my_theme_enqueue_styles() {
 
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', '1.0.1', true );
     wp_enqueue_script( 'child_scripts', get_theme_file_uri().'/assets/js/child-scripts.js', '', '1.0', true);
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
@@ -82,7 +82,7 @@ Custom Excerpt
 ============================================================================================*/
 function get_excerpt(){
 	$excerpt = get_the_content();
-	$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+	//$excerpt = preg_replace(" ([.*?])",'',$excerpt);
 	$excerpt = strip_shortcodes($excerpt);
 	$excerpt = strip_tags($excerpt);
 	$excerpt = substr($excerpt, 0, 200);
