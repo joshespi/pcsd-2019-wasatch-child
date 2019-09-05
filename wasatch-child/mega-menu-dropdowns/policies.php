f318b3e974d3d6ee73a01ed5078c88ce433a5164
Josh Espinoza <joshespi@gmial.com>
2019-09-05 10:53:18 -0600

    updated link to parent handbook

--- wasatch-child/mega-menu-dropdowns/policies.php
+++ wasatch-child/mega-menu-dropdowns/policies.php
@@ -1,7 +1,7 @@
 <div class="dropDownColumn">
 					<h3><img src="https://wasatch.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Policies</h3>
 						<ul>
-							<li class="pdf"><a href="https://www.wasatch.provo.edu/wp-content/uploads/2018/11/2017-Student_Parent-Handbook.pdf">Student Handbook</a></li>
+							<li class="pdf"><a href="https://wasatch.provo.edu/wp-content/uploads/2019/07/Parent-Handbook-2019-2020.pdf">Student Handbook</a></li>
 							<li class="pdf"><a href="https://provo.edu/wp-content/uploads/2018/08/08142018-3150-Student-Attendance.pdf">3150 Student Attendance</a></li>
 							<li class="pdf"><a href="https://provo.edu/wp-content/uploads/2017/01/3155-Releasing-Minors-from-School-Attendance.pdf">3155 Releasing Minors from School Attendance</a></li>
 			                <li class="pdf"><a href="https://provo.edu/wp-content/uploads/2018/08/08142018-3310-Safe-Schools-Policy.pdf">3310 Safe Schools</a></li>
